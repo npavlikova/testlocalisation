019c3dfe53f4e360462787cbb87a96782b202c7b
npavlikova <n.pavlikova@xsolla.com>
2019-07-17 16:50:42 +0500

    Update barmaglot.php

--- html/barmaglot.php
+++ html/barmaglot.php
@@ -1,42 +1,58 @@
-<p data-l10n-context="context" data-l10n-hint="hint">string</p>
-
-<h1>string</h1>
-
-<p>string</p>
-<p lang="">string</p>
-
-<img alt="string" title="string" src="..." />
-
-<p>
-    outer string
-    <span lang="">inner string</span>
-</p>
-<p>
-    <span lang="en">outer string</span>
-    <span lang="">inner string</span>
-</p>
-<p>
-    Click here: <a href="http://sample.com">http://sample.com</a>
-</p>
-<p>
-    <span lang="en">Click here:</span>
-    <a href="http://sample.com">http://sample.com</a>
-</p>
-
-<input type="search" placeholder="string">
-<input type="text" value="string">
-
-<div>string</div>
-<div lang="en">string</div>
-
-<?php
-    echo "string";
-    echo _("string");
-    echo __('string');
-    echo ___('string');
-?>
-
-<script type="text/javascript">
-    alert("string");
-    alert(___('string'));
-</script>
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  
+<title>Пример веб-страницы</title>
+ </head>
+ 
+<body>
+   <img alt="Бармаглот" src="https://dezinfo.net/images2/image/04.2010/narmag/1001.jpg" style="width: 900px; height: 900px;" />
+   
+<h1>Бармаглот</h1>
+   
+<div>Варкалось. Хливкие шорьки<br />
+   Пырялись по наве,<br />
+   И хрюкотали зелюки,<br />
+   Как мюмзики в мове.</div>
+   <br />
+   
+<div>О бойся Бармаглота, сын!<br />
+   Он так свирлеп и дик,<br />
+   А в глyще рымит исполин —<br />
+   Злопастный Брандашмыг.</div>
+   <br />
+   
+<div>Но взял он меч, и взял он щит,<br />
+   Высоких полон дyм.<br />
+   В глyщобy пyть его лежит<br />
+   Под дерево Тyмтyм.</div>
+   <br />
+   
+<div>Он стал под дерево и ждет,<br />
+   И вдрyг граахнyл гром —<br />
+   Летит yжасный Бармаглот<br />
+   И пылкает огнем!</div>
+   <br />
+   
+<div>Раз-два, раз-два! Горит трава,<br />
+   Взы-взы — стрижает меч,<br />
+   Ува! Ува! И голова<br />
+   Барабардает с плеч.</div>
+   <br />
+   
+<div>О светозарный мальчик мой!<br />
+   Ты победил в бою!<br />
+   О храброславленный герой,<br />
+   Хвалy тебе пою!</div>
+   <br />
+   
+<div>Варкалось. Хливкие шорьки<br />
+   Пырялись по наве,<br />
+   И хрюкотали зелюки,<br />
+   Как мюмзики в мове.</div>
+   <br />
+ 
+</body></html>
